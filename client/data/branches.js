dfb6be69df309bf4554b05850d586b573068f49e
Zahra Dargahi <zahra@mbp14.massive.com.au>
2017-05-16 13:49:55 +1000

    first commit

---
+++ client/data/branches.js
@@ -0,0 +1,43 @@
+const branches = {"type":"FeatureCollection",
+   "features":[  
+      {  
+         "type":"Feature",
+         "geometry":{  
+            "type":"Point",
+            "coordinates":[  
+               -84.46933,
+               39.25445
+            ]
+         },
+         "properties":{  
+            "cartodb_id":19,
+            "zip":45215,
+            "br_manager_phone":"(513) 782-4134",
+            "state":"OH",
+            "city":"Cincinnati",
+            "street_address":"10230 Springfield Pike",
+            "branch": "Roohani"
+         }
+      },
+      {  
+         "type":"Feature",
+         "geometry":{  
+            "type":"Point",
+            "coordinates":[  
+               -84.33962,
+               39.26043
+            ]
+         },
+         "properties":{  
+            "cartodb_id":7,
+            "zip":45242,
+            "br_manager_phone":"(513) 387-2109",
+            "state":"OH",
+            "city":"Cincinnati",
+            "street_address":"10851 Montgomery Rd",
+            "branch":"Montgomery"
+         }
+      }
+   ] 
+}     
+export default branches
